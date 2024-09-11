3c0380519c4d5ef91bfe1d1d411fcccbb57ef973
khalid karam <khalid.tasks@gmail.com>
2024-09-11 10:49:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PassByValueAndPassByReference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            //PassByValue
+            PassByValue(x);
+            Console.WriteLine(x);
+
+            //
+            PassByRefrence(ref x);
+            Console.WriteLine(x);
+        }
+
+        static void PassByValue(int x)
+        {
+            x = 90;
+        }
+
+        static void PassByRefrence(ref int x)
+        {
+            x = 100;
+        }
+
+
+    }
+}
